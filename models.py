c43b37eb5ec9979e4fd39b6b0f2320b6d0711cfd
Artem Frenk <97616103+afrenkai@users.noreply.github.com>
2024-10-10 10:15:11 -0400

    added misc stuff

--- models.py
+++ models.py
@@ -3,9 +3,9 @@ from sklearn.metrics import root_mean_squared_error, mean_absolute_error, r2_sco
 from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from harder_models import SGDRegressor, LSTM, DNN
+from tqdm import tqdm
 
-
-def model_picker(type: str, X_train, y_train, X_test, y_test) -> tuple:
+def model_picker(type: str, X_train, y_train, X_test, y_test):
     if X_train is None or y_train is None or X_test is None or y_test is None:
         return "Error: sm missin vro"
     model = None
@@ -14,6 +14,8 @@ def model_picker(type: str, X_train, y_train, X_test, y_test) -> tuple:
     r2 = None
     evs = None
     mse = None
+
+    macro = f' RMSE: {rmse}, R2: {r2}, MAE: {mae}, Explained Variance Score: {evs}, MSE: {mse}'
     if type == 'OLS Linear Regression':
         model = LinearRegression()
         model.fit(X_train, y_train)
@@ -23,17 +25,20 @@ def model_picker(type: str, X_train, y_train, X_test, y_test) -> tuple:
         mae = mean_absolute_error(y_test, y_hat)
         evs = explained_variance_score(y_test, y_hat)
         mse = rmse ** 2
-        return [(rmse, r2, mae, evs, mse)]
+        return macro
     elif type == 'Random Forest Regression':
-        model = RandomForestRegressor()
-        model.fit(X_train, y_train)
+        model = RandomForestRegressor(n_estimators=100, n_jobs=1)
+        with tqdm(total=model.n_estimators, desc="Training Random Forest", unit="tree") as pbar:
+            for i in range(model.n_estimators):
+                model.fit(X_train, y_train)
+                pbar.update(1)
         y_hat = model.predict(X_test)
         rmse = root_mean_squared_error(y_test, y_hat)
         r2 = r2_score(y_test, y_hat)
         mae = mean_absolute_error(y_test, y_hat)
         evs = explained_variance_score(y_test, y_hat)
         mse = rmse ** 2
-        return [(rmse, r2, mae, evs, mse)]
+        return macro
     elif type == 'Gradient Boost Regression':
         model = GradientBoostingRegressor(X_train, y_train)
         y_hat = model.predict(X_test)
@@ -42,7 +47,7 @@ def model_picker(type: str, X_train, y_train, X_test, y_test) -> tuple:
         mae = mean_absolute_error(y_test, y_hat)
         evs = explained_variance_score(y_test, y_hat)
         mse = rmse ** 2
-        return [(rmse, r2, mae, evs, mse)]
+        return macro
     elif type == 'Support Vector Regression':
         model = SVR(X_train, y_train)
         y_hat = model.predict(X_test)
@@ -51,7 +56,7 @@ def model_picker(type: str, X_train, y_train, X_test, y_test) -> tuple:
         mae = mean_absolute_error(y_test, y_hat)
         evs = explained_variance_score(y_test, y_hat)
         mse = rmse ** 2
-        return [(rmse, r2, mae, evs, mse)]
+        return macro
     elif type == 'Long Short Term Memory':
         # shape : [sample, timestep, feature]
         X_train_lstm = X_train.reshape((X_train.shape[0], 1, X_train.shape[1]))
@@ -64,7 +69,7 @@ def model_picker(type: str, X_train, y_train, X_test, y_test) -> tuple:
         mae = mean_absolute_error(y_test, y_hat)
         evs = explained_variance_score(y_test, y_hat)
         mse = rmse ** 2
-        return [(rmse, r2, mae, evs, mse)]
+        return macro
     elif type == 'SGD Linear Regression':
         model = SGDRegressor()
         model.fit(X_train, y_train)
@@ -74,7 +79,7 @@ def model_picker(type: str, X_train, y_train, X_test, y_test) -> tuple:
         mae = mean_absolute_error(y_test, y_hat)
         evs = explained_variance_score(y_test, y_hat)
         mse = model.mse(y_test, y_hat)
-        return [(rmse, r2, mae, evs, mse)]
+        return macro
     elif type == 'Deep Neural Network':
         model = DNN(layer_sizes=[X_train.shape[0], 64, 32, 1], eta=0.01)
         model.train(X_train, y_train, epochs=100)
@@ -84,6 +89,6 @@ def model_picker(type: str, X_train, y_train, X_test, y_test) -> tuple:
         mae = mean_absolute_error(y_test, y_hat)
         evs = explained_variance_score(y_test, y_hat)
         mse = rmse ** 2
-        return [(rmse, r2, mae, evs, mse)]
+        return macro
     else:
         return('not supported model or u buggin')
