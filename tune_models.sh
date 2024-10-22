d6c90e110992986e67fbe238aa318a859c566f2c
Sid Pradhan <64797131+SiddharthaPradhan@users.noreply.github.com>
2024-10-22 19:46:43 -0400

    added iforest outlier detection

---
+++ tune_models.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 20
+#SBATCH --mem=50
+#SBATCH -J "Tune Model"
+#SBATCH -p short
+#SBATCH -t 16:00:00
+#SBATCH --output=/home/sppradhan/KDDProject3/logs/TuneModels_%j.txt
+
+module load python/3.10.13
+source ~/KDDProject3/kdd/bin/activate
+cd model_tuning
+python tune_model.py
