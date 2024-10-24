9c5eedc0f0373c20e5fa88a1569700a8793dd933
Sid Pradhan <64797131+SiddharthaPradhan@users.noreply.github.com>
2024-10-24 01:40:59 -0400

    slurm push

--- tune_models.sh
+++ tune_models.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -n 32
-#SBATCH --mem=70g
+#SBATCH -n 36
+#SBATCH --mem=100g
 #SBATCH -J "Tune Model"
 #SBATCH -p short
-#SBATCH -t 16:00:00
+#SBATCH -t 20:00:00
 #SBATCH --output=/home/sppradhan/KDDProject3/logs/TuneModels_%j.txt
 
 module load python/3.10.13
