bf38c3bafd4ca26348e0c84b64aea32b388a4eb7
Sid Pradhan <64797131+SiddharthaPradhan@users.noreply.github.com>
2024-10-24 01:37:54 -0400

    updated model tuning

--- model_tuning/model_params.py
+++ model_tuning/model_params.py
@@ -1,37 +1,36 @@
+import numpy as np
 # RF
 RF_PARAMS = {
-    'n_estimators': [100, 200,300],
-    'max_depth': [None, 10, 20, 30],
+    'n_estimators': [200,300],
+    'max_depth': [20, 30, 40],
     'max_features': ['sqrt', 'log2'],
     'min_samples_split': [2, 5, 10],
     'min_samples_leaf': [1, 2, 4],
-    'n_jobs' : [1],
-    'random_state': [69]
+    'n_jobs' : [-1],
+    'random_state': [69],
 }
 
 RF_NAME = "Random Forest"
 
 # GB
-GB_PARAMS = {
-        'n_estimators': [200,300],
-        'max_depth': [3, 10, 20],
-        'max_features': ['sqrt', 'log2'],
-        'min_samples_split': [2, 5, 10],
-        'min_samples_leaf': [1, 2, 4],
+HGB_PARAMS = {
+        'max_depth': [None, 30, 40],
+        'max_features': list(np.linspace(0.5,1,3)),
+        'max_iter': [200, 300, 400],
+        'min_samples_leaf': [30, 40],
         'learning_rate' : [0.1, 0.01, 0.001],
         'random_state': [69]
 }
 
-GB_NAME = "Gradient Boosting"
+HGB_NAME = "Hist Gradient Boosting"
 
 # no tuning for OLS
 OLS_NAME = "OLS"
 
-# SVR
-SVR_PARAMS = {
-        'kernel': ['linear','poly', 'rbf'],
-        'degree': [2,3,4],
-        'C': [0.5, 0.8, 1],
+# ElasticNet 
+EN_NAME = "Elastic Net"
+# l1_ratio = 0 == L2 Pen, l1_ratio = 1 == L1 Pen
+EN_PARAMS = {
+        'l1_ratio': list(np.linspace(0.2,1,6)),
+        'random_state': [69]
 }
-
-SVR_NAME = "Support Vector"
