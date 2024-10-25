7b11cf2804e70354295765859231d4d177990718
Sid Pradhan <64797131+SiddharthaPradhan@users.noreply.github.com>
2024-10-25 02:02:12 -0400

    explain script

---
+++ explain.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 30
+#SBATCH --mem=100g
+#SBATCH -J "Shap explain"
+#SBATCH -p short
+#SBATCH -t 20:00:00
+#SBATCH --output=/home/sppradhan/KDDProject3/logs/Explain_%j.txt
+
+module load python/3.10.13
+source ~/KDDProject3/kdd/bin/activate
+cd explainability
+python explain.py
